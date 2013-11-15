8a4fea937653c738153540b6d00d1e7a8f43e6b6
Lukas Häfliger <haelukas@student.ethz.ch>
2013-11-15 17:02:36 +0100

    make screen fullscreen

--- Project/src/ch/ethz/inf/vs/android/haelukas/project/StartUpActivity.java
+++ Project/src/ch/ethz/inf/vs/android/haelukas/project/StartUpActivity.java
@@ -5,6 +5,8 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StartUpActivity extends Activity {
 
@@ -15,6 +17,10 @@ public class StartUpActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		startUpGLView = new StartUpGLView(this);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(
+		    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		    WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(startUpGLView);
 	}
 
