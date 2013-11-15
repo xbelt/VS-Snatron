d23d0e720a0814c5000cfde3cddd1758492b3c89
Lukas Häfliger <haelukas@student.ethz.ch>
2013-11-15 16:32:14 +0100

    add StartUpGLView

---
+++ Project/src/ch/ethz/inf/vs/android/haelukas/project/StartUpGLView.java
@@ -0,0 +1,13 @@
+package ch.ethz.inf.vs.android.haelukas.project;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class StartUpGLView extends GLSurfaceView {
+
+	public StartUpGLView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
