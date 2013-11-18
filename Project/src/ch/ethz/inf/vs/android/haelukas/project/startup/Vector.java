da201846507324a1fe329d91e36672473a0adccb
Lukas Häfliger <haelukas@student.ethz.ch>
2013-11-18 16:20:42 +0100

    add Vector

---
+++ Project/src/ch/ethz/inf/vs/android/haelukas/project/startup/Vector.java
@@ -0,0 +1,13 @@
+package ch.ethz.inf.vs.android.haelukas.project.startup;
+
+public class Vector {
+	private float x;
+	private float y;
+	private float z;
+	
+	public Vector(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
