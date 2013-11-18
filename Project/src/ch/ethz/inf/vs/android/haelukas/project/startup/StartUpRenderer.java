ecedc201f37f64a418658abfc3301c3c0c20d5a3
Lukas Häfliger <haelukas@student.ethz.ch>
2013-11-18 15:22:44 +0100

    add some drawing

--- Project/src/ch/ethz/inf/vs/android/haelukas/project/startup/StartUpRenderer.java
+++ Project/src/ch/ethz/inf/vs/android/haelukas/project/startup/StartUpRenderer.java
@@ -5,21 +5,83 @@ import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.Matrix;
+import android.util.Log;
 
 public class StartUpRenderer implements Renderer {
 
-	@Override
-	public void onSurfaceCreated(GL10 arg0, EGLConfig arg1) {
-		GLES20.glClearColor(0f, 0f, 1f, 1f);
-	}
+	 private Rectangle mRectangle;
+	 private Grid grid;// = new Grid();
+	private float[] mProjectionMatrix = new float[16];
+	private float[] mViewMatrix = new float[16];
+	private float[] mMVPMatrix = new float[16];
+	
+	private float y = 0;
 
-	@Override
-	public void onDrawFrame(GL10 arg0) {
-		 GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-	}
+    @Override
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
 
-	@Override
-	public void onSurfaceChanged(GL10 arg0, int width, int height) {
-		GLES20.glViewport(0, 0, width, height);
-	}
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        grid = new Grid();
+        //mRectangle = new Rectangle(0,0);
+        //mTriangle = new Triangle();
+    }
+
+    @Override
+    public void onDrawFrame(GL10 unused) {
+
+        // Draw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        
+		Matrix.setLookAtM(mViewMatrix, 0, 3, 1, 3, 0f, 0f, 0f, 0f, 1f, 0f);
+//        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+        
+        // Draw triangle
+//        mRectangle.draw(mMVPMatrix);
+        grid.draw(mViewMatrix);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        // Adjust the viewport based on geometry changes,
+        // such as screen rotation
+        GLES20.glViewport(0, 0, width, height);
+        
+        float ratio = (float) width / height;
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 5, 30);
+    }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+    
+    /**
+     * Utility method for debugging OpenGL calls. Provide the name of the call
+     * just after making it:
+     *
+     * <pre>
+     * mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+     * MyGLRenderer.checkGlError("glGetUniformLocation");</pre>
+     *
+     * If the operation is not successful, the check throws an error.
+     *
+     * @param glOperation - Name of the OpenGL call to check.
+     */
+    public static void checkGlError(String glOperation) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e("StartUpRenderer", glOperation + ": glError " + error);
+            throw new RuntimeException(glOperation + ": glError " + error);
+        }
+    }
 }
