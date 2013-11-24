d6dad5e8102ba92cddc17313474ad360a0c23ea3
Marko Zivkovic <markoz@student.ethz.ch>
2013-11-24 05:20:44 +0100

    Added Game Controller

---
+++ Project/src/ch/ethz/inf/vs/android/haelukas/project/game/GameController.java
@@ -0,0 +1,17 @@
+package ch.ethz.inf.vs.android.haelukas.project.game;
+
+public class GameController {
+
+	public GameController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
