1ff84ce6414876fd719fbdc67ba283abec017a65
Lukas Häfliger <haelukas@student.ethz.ch>
2013-11-25 14:44:34 +0100

    some GUI testelements added

---
+++ Project/Assets/GUI_Control.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_Control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	private void OnGUI() {
+		GUI.Label (new Rect (10, 10, 100, 40), "welcome to Snatron");
+		GUI.Button (new Rect (25, 75, 100, 30), "START!!");
+	}
+
+}
