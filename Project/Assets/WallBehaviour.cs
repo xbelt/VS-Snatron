75f5683692621f61529e4637299354e80345ea09
Lukas Häfliger <haelukas@student.ethz.ch>
2013-11-27 15:49:50 +0100

    [FIX] epsilon in WallBehaviour.cs

--- Project/Assets/WallBehaviour.cs
+++ Project/Assets/WallBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class WallBehaviour : MonoBehaviour {
@@ -12,7 +13,7 @@ public class WallBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	public void updateWall (Vector3 newEnd) {
 		end = newEnd;
-		float angle = (start.x == end.x) ? 0 : 90;
+		float angle = (Math.Abs(start.x - end.x) < 0.001) ? 0 : 90;
 		transform.position = Vector3.Lerp(start, end, 0.5f) + Vector3.up * 1f;
 		transform.eulerAngles = new Vector3(0, angle, 0);
 		transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Vector3.Distance(start, end));
