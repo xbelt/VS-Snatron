763062f486768577cb85e0d1e0366cfe8386992b
Lukas Häfliger <haelukas@student.ethz.ch>
2013-11-27 17:37:12 +0100

    start on networking

--- Project/Assets/GUI_Control.cs
+++ Project/Assets/GUI_Control.cs
@@ -1,11 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Assets;
+using UnityEngine;
 using System.Collections;
 
 public class GUI_Control : MonoBehaviour {
+    public Boolean isDiscovering = true;
+    private List<String> servers = new List<String>();
 
-	// Use this for initialization
+    private IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Broadcast, Protocol.serverPort);
+    UdpClient udpClient = new UdpClient();
+
+    Vector2 scrollPosition = Vector2.zero;
+    // Use this for initialization
 	void Start () {
-	
+        var sendBytes4 = Encoding.ASCII.GetBytes(Protocol.Discover);
+        udpClient.Send(sendBytes4, sendBytes4.Length, ipEndPoint);
+
+        (new Thread(() =>
+        {
+            while (isDiscovering)
+            {
+                var endPoint = new IPEndPoint(IPAddress.Any, Protocol.serverPort);
+                var result = udpClient.Receive(ref endPoint);
+                var message = Encoding.ASCII.GetString(result);
+                servers.Add(message);
+            }
+        })).Start();
 	}
 	
 	// Update is called once per frame
@@ -16,14 +41,37 @@ public class GUI_Control : MonoBehaviour {
         if (GUI.Button(new Rect(25, 25, 100, 30), "Host"))
         {
         }
-        if (GUI.Button(new Rect(25, 75, 100, 30), "Join"))
-        {
-        }
-        if (GUI.Button(new Rect(25, 125, 100, 30), "Race"))
-        {
+        if (GUI.Button(new Rect(25, 75, 100, 30), "Race")) {
+            isDiscovering = false;
+            udpClient.Close();
             Application.LoadLevel(1);
         }
+        
+        GUILayout.BeginArea(new Rect(150f, 25f, 300f, 200f), GUI.skin.window);
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
+        GUILayout.BeginVertical(GUI.skin.box);
+
+        foreach (string item in servers)
+        {
+            if (GUILayout.Button(item, GUI.skin.box, GUILayout.ExpandWidth(true))) {
+                Application.LoadLevel(1);
+            }
+        }
+
+        GUILayout.EndVertical();
+        GUILayout.EndScrollView();
+        GUILayout.EndArea();
 	}
+
+}
+
+
+internal class GameServer {
+    private IPAddress _ipAddress;
+
+    public GameServer(IPAddress address) {
+        _ipAddress = address;
+    }
 
 }
 
---
+++ Project/Assets/Protocol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    static class Protocol {
+        public static int serverPort = 10100;
+        public static string Discover = "Discover";
+    }
+}
