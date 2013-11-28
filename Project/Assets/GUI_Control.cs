bd97355cf81a9b139dca3d83b1d8a4c122da7384
Lukas Häfliger <haelukas@student.ethz.ch>
2013-11-28 19:53:06 +0100

    implement working server detection and hosting

--- Project/Assets/GUI_Control.cs
+++ Project/Assets/GUI_Control.cs
@@ -2,51 +2,40 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using Assets;
 using UnityEngine;
-using System.Collections;
 
 public class GUI_Control : MonoBehaviour {
+    private readonly List<Server> _servers = new List<Server>();
+    private bool isSearching = true;
 
-    public Boolean isDiscovering = true;
-    private List<String> servers = new List<String>();
-
-    private IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Broadcast, Protocol.serverPort);
-    UdpClient udpClient = new UdpClient();
-
-    Vector2 scrollPosition = Vector2.zero;
+    Vector2 _scrollPosition = Vector2.zero;
 
 
     // Use this for initialization
 	void Start () {
-        /*var sendBytes4 = Encoding.ASCII.GetBytes(Protocol.Discover);
-        udpClient.Send(sendBytes4, sendBytes4.Length, ipEndPoint);
-
         (new Thread(() =>
-        {
-            while (isDiscovering)
-            {
-                Socket sock = new Socket(AddressFamily.InterNetwork,
-                      SocketType.Dgram, ProtocolType.Udp);
-                IPEndPoint iep = new IPEndPoint(IPAddress.Any, Protocol.serverPort);
-                sock.Bind(iep);
-                EndPoint ep = (EndPoint)iep;
-                Console.WriteLine("Ready to receive...");
-
-                byte[] data = new byte[1024];
-                int recv = sock.ReceiveFrom(data, ref ep);
-                string stringData = Encoding.ASCII.GetString(data, 0, recv);
-                Console.WriteLine("received: {0}  from: {1}",
-                                      stringData, ep.ToString());
-                servers.Add(stringData);
-                /*var endPoint = new IPEndPoint(IPAddress.Any, Protocol.serverPort);
-                var result = udpClient.Receive(ref endPoint);
-                var message = Encoding.ASCII.GetString(result);
-                servers.Add(message);
-            }
-        })).Start();*/
+	    {
+	        while (isSearching)
+	        {
+                Debug.Log("First line");
+	            var newServer = ServerDiscoverer.DiscoverServers();
+                Debug.Log("Got new server");
+	            bool addServer = true;
+	            foreach (var server in _servers)
+	            {
+	                if (server.Name.Equals(newServer.Name))
+	                {
+	                    addServer = false;
+	                }
+	            }
+	            if (addServer && newServer != null && newServer.Name != null)
+	            {
+	                _servers.Add(newServer);
+	            }
+	        }
+	    })).Start();
 	}
 	
 	// Update is called once per frame
@@ -54,22 +43,27 @@ public class GUI_Control : MonoBehaviour {
 	
 	}
 	private void OnGUI() {
-        if (GUI.Button(new Rect(25, 25, 100, 30), "Host"))
-        {
-        }
+	    if (GUI.Button(new Rect(25, 25, 100, 30), "Host"))
+	    {
+            //TODO: replace with chosen name
+	        ServerHoster.HostServer("testName2");
+	    }
         if (GUI.Button(new Rect(25, 75, 100, 30), "Race")) {
-            isDiscovering = false;
-            udpClient.Close();
+            isSearching = false;
+            ServerHoster.IsHosting = false;
             Application.LoadLevel(1);
         }
         
         GUILayout.BeginArea(new Rect(150f, 25f, 300f, 200f), GUI.skin.window);
-        scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
+        _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, false, true);
         GUILayout.BeginVertical(GUI.skin.box);
 
-        foreach (string item in servers)
+        foreach (var item in _servers)
         {
-            if (GUILayout.Button(item, GUI.skin.box, GUILayout.ExpandWidth(true))) {
+            if (GUILayout.Button(item.Name, GUI.skin.box, GUILayout.ExpandWidth(true)))
+            {
+                isSearching = false;
+                ServerHoster.IsHosting = false;
                 Application.LoadLevel(1);
             }
         }
