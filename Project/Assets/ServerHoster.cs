c93ab36ff4ba4509d7390b3d80e1f84fe5aa4d13
Lukas Häfliger <haelukas@student.ethz.ch>
2013-11-28 19:55:22 +0100

    implement ServerHoster inculding correct serialization

--- Project/Assets/ServerHoster.cs
+++ Project/Assets/ServerHoster.cs
@@ -1,19 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Xml.Serialization;
 
 namespace Assets
 {
     class ServerHoster
     {
-        public static void hostServer(String hostname) {
+        public static int Timeout = 2000;
+        public static bool IsHosting = true;
+
+        public static void HostServer(String hostname) {
             var ipEndPoint = new IPEndPoint(IPAddress.Broadcast, Protocol.serverPort);
             var udpClient = new UdpClient();
-            var sendBytes4 = Encoding.ASCII.GetBytes();
-            udpClient.Send(sendBytes4, sendBytes4.Length, ipEndPoint);
+
+            var message = new ServerMessage(Protocol.serverPort, hostname);
+            //Serialize message
+            var serializer = new XmlSerializer(message.GetType());
+            using (var writer = new StringWriter())
+            {
+                serializer.Serialize(writer, message);
+                var sendBytes4 = Encoding.ASCII.GetBytes(writer.ToString());
+                (new Thread(() =>
+                {
+                    while (IsHosting)
+                    {
+                        udpClient.Send(sendBytes4, sendBytes4.Length, ipEndPoint);
+                        Thread.Sleep(Timeout);
+                    }
+                    udpClient.Close();
+                })).Start();
+                
+            }
         }
     }
 }
