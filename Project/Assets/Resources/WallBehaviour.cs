d7ca8d529be12c242291fbddff82c657b135f456
Alexander Viand <AlexanderViand@gmail.com>
2013-12-15 19:32:47 +0100

    Clean up WallBehaviour (unused functions)

--- Project/Assets/Resources/WallBehaviour.cs
+++ Project/Assets/Resources/WallBehaviour.cs
@@ -4,23 +4,7 @@ using UnityEngine;
 public class WallBehaviour : MonoBehaviour {
 	public Vector3 start;
 	public Vector3 end;
-    private Color DefaultColor = new Color(255/255f,255/255f,255/255f,0f);
-	// Use this for initialization
-	void Start ()
-	{
-        //gameObject.renderer.material.color = DefaultColor;
-	}
-
-    void Update()
-    {
-    }
-
-    public void setDefaultColor(Color c)
-    {
-        DefaultColor = c;
-    }
 	
-	// Update is called once per frame
 	public void updateWall (Vector3 newEnd) {
 		end = newEnd;
 		float angle = (Math.Abs(start.x - end.x) < 0.001) ? 0 : 90;
