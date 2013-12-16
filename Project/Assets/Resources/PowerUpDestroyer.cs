369403488dd600b80bca997490aeb9b16e0b33f7
Lukas Häfliger <haelukas@student.ethz.ch>
2013-12-16 16:12:07 +0100

    add PowerUpDestroyer script

---
+++ Project/Assets/Resources/PowerUpDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpDestroyer : MonoBehaviour {
+    private System.Random random = new System.Random();
+	// Use this for initialization
+	void Start ()
+	{
+	    var ttl = random.Next(5, 15);
+        Destroy(gameObject, ttl);
+	}
+}
