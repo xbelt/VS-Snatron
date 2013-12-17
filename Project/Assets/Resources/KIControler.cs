f6abf20604d2e182fa16f276d88362ec82445622
Lukas Häfliger <haelukas@student.ethz.ch>
2013-12-17 17:58:28 +0100

    add KIControler with basic KI

---
+++ Project/Assets/Resources/KIControler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.Resources
+{
+    class KIControler : Drive
+    {
+        private System.Random random = new Random();
+        public int KIId;
+        void Start() {
+            transform.FindChild("CollisionPredictor").GetComponent<CollisionPrediction>()._drive = this;
+            if (GetComponent<NetworkView>().isMine)
+            {
+                NewWall();
+            }
+        }
+
+        void Update() {
+            if (GetComponent<NetworkView>().isMine)
+            {
+                // Note: For the collision detection to work well,
+                // it is essential that the tron rests at the same place
+                // when it is turning. Otherwise the engine has no time to
+                // predict the collision. Therefore we return if there was some user input
+                bool applied = ApplyUserCommands();
+                if (applied)
+                    return;
+            }
+
+            // The tron would keep moving straight
+            // But are there any obstacles in front?
+            if (_predictedCollisions > 0 && !isIndestructible || _predictedWallCollisions > 0)
+            {
+                if (random.Next(0, 100) < 98) {
+                    if (random.Next(0, 100) < 50) {
+                        TurnLeft();
+                    }
+                    else {
+                        TurnRight();
+                    }
+                }
+                else {
+                    Debug.Log("KI" + KIId + " died");
+                    Kill();
+                }
+                return;
+            }
+
+            // move forward
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+            if (_latestWallGameObject != null)
+            {
+                _latestWallGameObject.GetComponent<WallBehaviour>().updateWall(CurrentWallEnd);
+            }
+
+            // Adjust the speed last. Implies resizing collider for collision prediction.
+            // want to resize the collider before control is returned to unity for collision detection etc.
+            // Adjusting the collider within this method, earlier, would not immediately trigger new collisions etc.
+            AdjustSpeed();
+        }
+
+        new bool ApplyUserCommands() {
+            //Handling touch input
+            if (random.Next(0, 1000) < 5) {
+                TurnLeft();
+                return true;
+            }
+            if (random.Next(0, 1000) > 994) {
+                TurnRight();
+                return true;
+            }
+            return false;
+        }
+
+        protected override void Kill() {
+            _latestWallGameObject.GetComponent<WallBehaviour>().updateWall(transform.position);
+            Network.Destroy(gameObject);
+        }
+
+        protected override void NewWall() {
+            _latestWallGameObject = ((GameObject)Network.Instantiate(UnityEngine.Resources.Load("Wall" + KIId), CurrentWallEnd, Quaternion.identity, 0)).transform;
+            _latestWallGameObject.GetComponent<WallBehaviour>().start = CurrentWallEnd;
+            _latestWallGameObject.GetComponent<WallBehaviour>().end = CurrentWallEnd;
+            _latestWallGameObject.GetComponent<WallBehaviour>().updateWall(CurrentWallEnd);
+        }
+
+    }
+}
