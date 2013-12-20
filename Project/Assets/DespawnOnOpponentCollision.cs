3f9af3952de0c0b755db4938bca44ab879a618d5
Christian Vonrüti <christian@vonrueti.ch>
2013-12-20 02:10:57 +0100

    Add DespawnOnOpponentCollision

---
+++ Project/Assets/DespawnOnOpponentCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DespawnOnOpponentCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "tron") {
+            if (other.gameObject != Game.Instance.Spawner.LocalPlayer) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
